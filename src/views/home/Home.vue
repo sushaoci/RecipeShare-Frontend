8d7750fcb4e9c268eabe6832e8885c2dc2650819
sushaoci <sushaociqiqi@gmail.com>
2019-12-08 19:05:16 +0800

    profile组件写完了,路由最后再调,所以现在跳转还没写

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <el-col :span="4">
+    <el-col :span="6">
+      <Profile :profile="profile"/>
+    </el-col>
+
+    <!-- <el-col :span="4">
       <LeftFilter :tags="tags" @filter="filter(arguments)" />
     </el-col>
 
@@ -8,26 +12,31 @@
       <div v-for="recipe in recipeShow">
         <RecipeCard :url="recipe.url" :title="recipe.title" :author="recipe.author" />
       </div>
-    </el-col>
+    </el-col>-->
   </div>
 </template>
 
 <script>
 import recipes from "@/data/recipes.json";
 import tags from "@/data/tags.json";
+import profile from '@/data/profile.json'
+
 import RecipeCard from "./components/recipeCard";
 import LeftFilter from "./components/leftFilter";
+import Profile from "./components/rightProfile";
 
 export default {
   components: {
     RecipeCard,
-    LeftFilter
+    LeftFilter,
+    Profile
   },
   data() {
     return {
       recipes: [],
       tags: [],
-      recipeShow: []
+      recipeShow: [],
+      profile:[]
     };
   },
   methods: {
@@ -35,6 +44,7 @@ export default {
       this.recipes = recipes.recipes;
       this.tags = tags.tags;
       this.recipeShow = recipes.recipes;
+      this.profile = profile.profile;
     },
     filter(vals) {
       this.recipeShow = [];
---
+++ src/views/home/components/rightProfile.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-card class="box-card">
+    <div>
+      <el-avatar :src="profile.avatar"></el-avatar>
+      <h1>{{profile.name}}</h1>
+      <el-row>
+        <el-col :span="12">{{profile.likes}} 喜欢</el-col>
+        <el-col :span="12">{{profile.creates}} 创建</el-col>
+      </el-row>
+      <el-row>
+          <el-button>创建菜单</el-button>
+      </el-row>
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  props: {
+    profile: {
+      avatar: String,
+      name: String,
+      likes: Number,
+      creates: Number
+    }
+  }
+};
+</script>
+
+<style>
+</style>
