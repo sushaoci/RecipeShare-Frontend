91bd67ead427f0e4ef719ad2576ca58efe242438
sushaoci <sushaociqiqi@gmail.com>
2019-12-10 19:57:38 +0800

    添加了菜谱详情页

---
+++ src/views/recipeDetail/components/comments.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <p>{{commentsNum}} 评论</p>
+    <el-row>
+      <el-col :span="2">
+        <el-avatar :src="avatar"></el-avatar>
+      </el-col>
+      <el-col :span="18">
+        <el-input v-model="input" placeholder="添加评论"></el-input>
+      </el-col>
+      <el-col :span="4">
+        <el-button @click="onclick">提交</el-button>
+      </el-col>
+    </el-row>
+    <div v-for="i in comments">
+      <Comment :avatar="i.avatar" :name="i.name" :time="i.time" :content="i.content" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Comment from "./comment";
+export default {
+  components: {
+    Comment
+  },
+  props: {
+    avatar: String,
+    comments: Array
+  },
+  data() {
+    return {
+      input: ""
+    };
+  },
+  computed: {
+    commentsNum() {
+      return this.comments.length;
+    }
+  },
+  methods: {
+    onclick() {
+    //   console.log('zi',this.input);
+      this.$emit("clicked",this.input);
+      this.input = "";
+    }
+  }
+};
+</script>
+
+<style>
+</style>
